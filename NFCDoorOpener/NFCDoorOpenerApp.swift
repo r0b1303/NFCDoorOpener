fc46d7bf71f5394a41b8244cd2aa4e1dad3e79d0
Robin Schnaiter <robinleon1303@icloud.com>
2024-03-13 00:17:48 +0100

    Initial Commit

---
+++ NFCDoorOpener/NFCDoorOpenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCDoorOpenerApp.swift
+//  NFCDoorOpener
+//
+//  Created by Robin Schnaiter on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct NFCDoorOpenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
